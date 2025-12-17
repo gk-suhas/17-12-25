99f0974be2d296c049ce1a65564b4b60ad77aa84
gk-suhas <karanamsuhas80@gmail.com>
2025-12-17 10:06:36 +0530

    first commit

--- App.jsx
+++ App.jsx
@@ -1,13 +1,25 @@
-import React from 'react'
-import userContext from './UserContext'
-import Profile from './Profile'
+import React,{useReducer} from 'react'
+
 function App() {
-  const value="suhas"
+  const[state,dispatch]=useReducer(reducer,{count:0})
+
   return (
-    <userContext.Provider value="suhas">
-      <Profile/>
-    </userContext.Provider>
+    <>
+    <h3>{state.count}</h3>
+    <button onClick={()=>dispatch({type:'increment'})}>+</button>
+    <button onClick={()=>dispatch({type:"decrement"})}>-</button>
+    <button onClick={()=>dispatch({type:"reset"})}>reset</button>
+    </>
   )
 }
 
 export default App
+
+function reducer(state,action){
+     switch(action.type){
+      case 'increment':return {count: state.count+1}
+      case 'decrement':return {count: state.count-1}
+      case 'reset': return {count: 0}
+      default :return state
+     }
+}
